9a53401c4594bd6aef696738b2d27227e8e0c0dc
LuisFernandoBS <luisfernando142536@gmail.com>
2026-02-25 15:42:11 -0300

    Implementando favicons

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,16 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
+  app: {
+    head: {
+      link: [
+        { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon/favicon-16x16.png' },
+        { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon/favicon-32x32.png' },
+        { rel: 'apple-touch-icon', sizes: '180x180', href: '/favicon/apple-touch-icon.png' },
+        { rel: 'manifest', href: '/favicon/site.webmanifest' }
+      ]
+    }
+  },
   css: ['~/assets/css/main.css'],
   modules: [
     '@nuxt/icon',
@@ -34,9 +44,14 @@ export default defineNuxtConfig({
       start_url: '/',
       icons: [
         {
-          src: '/favicon.ico',
-          sizes: '64x64 32x32 24x24 16x16',
-          type: 'image/x-icon'
+          src: '/favicon/android-chrome-192x192.png',
+          sizes: '192x192',
+          type: 'image/png'
+        },
+        {
+          src: '/favicon/android-chrome-512x512.png',
+          sizes: '512x512',
+          type: 'image/png'
         }
       ]
     },
